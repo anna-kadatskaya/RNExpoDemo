fbddce212605abaf3d2b3bdcca4850d0e61ada41
anna.kadatskaya <anna.kadatskaya@codemagic.io>
2025-06-25 08:26:05 +0200

    code signing changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
+import { Image } from 'expo-image';
 import { StyleSheet, Text, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -17,5 +23,13 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
